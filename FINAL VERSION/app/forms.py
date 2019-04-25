3f7d1cdd4df35717a71b0cefb98e0009b27a9fed
VivianGunawan <42987601+VivianGunawan@users.noreply.github.com>
2019-04-25 03:51:36 -0400

    Update forms.py
    add new forms

--- FINAL VERSION/app/forms.py
+++ FINAL VERSION/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, InputRequired
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -12,10 +12,12 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    name = StringField('Name', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Register')
 
     def validate_username(self, username):
@@ -52,3 +54,7 @@ class EatForm(FlaskForm):
     food = StringField('Food', validators=[DataRequired()])
     submit = SubmitField('Find Food')
     
+class InviteForm(FlaskForm):
+    friends = SelectField('Invite: ', choices=[] ,coerce=int,validators=[InputRequired()])
+    food = SelectField('To: ', choices=[] ,coerce=int,validators=[InputRequired()])
+    submit = SubmitField('Invite')
